32b07ad5b17e4ee6bea0215bdfe5a2cebc76452f
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-27 15:08:12 -0600

    Demo finished

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ let resetBtn = document.querySelector('#reset-btn')
 let counterElement = document.querySelector('#counter')
 
 
+let buttons = document.querySelectorAll('.theme-buttons')
+
+
+
+
 function increase() {
     count++
     counterElement.innerHTML = count
@@ -27,6 +32,29 @@ minusBtn.addEventListener('click', decrease);
 plusBtn.addEventListener('click', increase);
 resetBtn.addEventListener('click', reset);
 
+
+function changeTheme(event) {
+   let classString = event.target.innerHTML;
+
+   document.querySelector('body').className = classString
+   document.querySelector('main').className = classString
+
+   let allButtons = document.querySelectorAll('button')
+
+   for (let i = 0; i < allButtons.length; i++) {
+    let currentButton = allButtons[i]
+    currentButton.classList = classString
+    
+   }
+
+}
+
+
+for (let i = 0; i < buttons.length; i++) {
+    let currentBtn = buttons[i]
+
+    currentBtn.addEventListener('click', changeTheme)
+}
 
 
 
